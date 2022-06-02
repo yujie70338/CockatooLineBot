1702b7f391ecfef01c0383baffa42a928b4035c2
yujie <96967477+yujie70338@users.noreply.github.com>
2022-06-02 10:54:25 +0800

    update app.py

--- app.py
+++ app.py
@@ -14,8 +14,7 @@ line_bot_api = LineBotApi('Chennel access token')
 handler = WebhookHandler('Channel secret')
 # 聊天機器人的 Channel secret
 
-# 接收 LINE 的資訊
-# 接收 LINE 的資訊
+# 建立callback路由，檢查LineBot資訊是否正確
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -27,18 +26,22 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-# 學你說話
-@handler.add(MessageEvent, message=TextMessage)
-def echo(event):
+# 學你說話程式碼
+@handler.add(MessageEvent, message=TextMessage) 
+# 當收到LINE的MessageEvent(信息事件)，而且信息是屬於TextMessage，執行。
+# LINE的事件有：MessageEvent(信息事件)、FollowEvent(加好友事件)、UnfollowEvent(刪好友事件)、JoinEvent(加入聊天室事件)、LeaveEvent(離開聊天室事件)、MemberJoinedEvent(加入群組事件)、MemberLeftEvent(離開群組事件)
+
+def echo(event): #echo函數會接收一個LINE發送過來的資訊，並貼上event的標籤。
     
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef": # 如果是Line官方傳的測試訊息，就不回應
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text)
         )
+        # reply_message方法為回傳訊息給使用者，push_message為主動推播訊息給使用者
+        # 可以回傳的種類有Text, Image, Video, Audio, Location, Sticker, Template等等
 
 if __name__ == "__main__":
     app.run()
